8c0e9cb6ef88a84900e1a5179ad3c69a38ddee1f
levAriUlnerWork <levariulner@gmail.com>
2022-01-06 12:06:19 +0200

    Little Change

--- ML Models Structure.py
+++ ML Models Structure.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 import os
 import joblib
@@ -49,25 +50,24 @@ class ClassifierModel:
     # ****************** MODELS: ************************************
 
     def ANN(self):
-      ANN_Classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
-      ANN_Classifier.fit(self.X,self.y)
-      y_pred = ANN_Classifier.predict(self.X_test)
+        ANN_Classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+        ANN_Classifier.fit(self.X,self.y)
+        y_pred = ANN_Classifier.predict(self.X_test)
 
-      print("\n")
-      print("************************* Nueral Network Classifier ************************* \n")
-      print('Classification Report: ')
-      print(classification_report(self.y_test, y_pred), '\n')
-      print('Confusion Matrix: ')
-      print(confusion_matrix(self.y_test, y_pred), '\n')
-      print('Precision: ', self.accuracy(confusion_matrix(self.y_test, y_pred)) * 100, '%')
+        print("\n")
+        print("************************* Nueral Network Classifier ************************* \n")
+        print('Classification Report: ')
+        print(classification_report(self.y_test, y_pred), '\n')
+        print('Confusion Matrix: ')
+        print(confusion_matrix(self.y_test, y_pred), '\n')
+        print('Precision: ', self.accuracy(confusion_matrix(self.y_test, y_pred)) * 100, '%')
 
-      self.classification_report_plot(classification_report(self.y_test, y_pred,output_dict=True), "RF")
+        self.classification_report_plot(classification_report(self.y_test, y_pred,output_dict=True), "RF")
 
-      if len(self.X_train[0]) == 2:
-          self.classification_view(self.X_train, self.y_train, ANN_Classifier)
+        if len(self.X_train[0]) == 2:
+            self.classification_view(self.X_train, self.y_train, ANN_Classifier)
 
     def SVM(self, kernel_type):
-        from sklearn.svm import SVC
         SVM_Classifier = SVC(kernel=kernel_type)
         SVM_Classifier.fit(self.X_train, self.y_train)
         y_pred = SVM_Classifier.predict(self.X_test)
