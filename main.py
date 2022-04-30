dc6e34252c4a72d0716661f10384850e62e089f8
Gitthusiast <dorfmag@epost.bgu.ac.il>
2022-04-30 13:13:31 +0300

    restore main

--- main.py
+++ main.py
@@ -1,67 +1,147 @@
-import os
-import numpy as np
+### Example Inputs ###
+# python main.py -file thursday-100M-v2.csv -features 4,5,6,8,20,22,23,24,25,26,27,29 -label 32 -test 0.2
+# python main.py -file thursday-100M-v2.csv -features 1,5,8,13,17,18,19,20,22,23,24,25,26,29 -label 32 -test 0.2
+### End ###
+
+
 import pandas as pd
+import sys
+from ML_Models_Structure import ClassifierModel
+from traffic_data_preprocess import data_preprocess, add_label
 
-directory = '.\\real-traffic'
+# known OS (source: https://www.unb.ca/cic/datasets/ids-2017.html)
+"""ip_dict = {
+    '192.168.10.51': 'Ubuntu server 12',
+    '192.168.10.19': 'Ubuntu 14.4',
+    '192.168.10.17': 'Ubuntu 14.4',
+    '192.168.10.16': 'Ubuntu 16.4',
+    '192.168.10.12': 'Ubuntu 16.4',
+    '192.168.10.9': 'Win 7',
+    '192.168.10.5': 'Win 8.1',
+    '192.168.10.8': 'Win Vista',
+    '192.168.10.14': 'Win 10',
+    '192.168.10.15': 'Win 10',
+    '192.168.10.25': 'macOS'
+}"""
 
+# Consider all Ubuntu versions as Ubuntu
+# ip_dict = {
+#     '192.168.10.51': 'Ubuntu',
+#     '192.168.10.19': 'Ubuntu',
+#     '192.168.10.17': 'Ubuntu',
+#     '192.168.10.16': 'Ubuntu',
+#     '192.168.10.12': 'Ubuntu',
+#     '192.168.10.9': 'Win 7',
+#     '192.168.10.5': 'Win 8.1',
+#     '192.168.10.8': 'Win Vista',
+#     '192.168.10.14': 'Win 10',
+#     '192.168.10.15': 'Win 10',
+#     '192.168.10.25': 'macOS'
+# }
 
+# Consider all Ubuntu versions as Ubuntu and Windows as Windows
 ip_dict = {
-    '10.0.0.10': 'Win 10',
-    '132.73.223.74': 'Win 10',
-    '10.100.102.8': 'Rhel 8.5',
-    '10.100.102.7': 'Ubuntu 18.04',
-    '10.0.0.7': 'ARCH',
-    '192.168.1.81': 'Mac 2017',
-    '192.168.1.34': 'Win 11',
-    '192.168.1.105': 'Win 11',
-    '10.0.0.6': 'Win 7',
-    '10.0.0.4': 'Win 10',
-    '192.168.1.56': 'Mac 2019',
+     '192.168.10.51': 'Ubuntu',
+     '192.168.10.19': 'Ubuntu',
+     '192.168.10.17': 'Ubuntu',
+     '192.168.10.16': 'Ubuntu',
+     '192.168.10.12': 'Ubuntu',
+     '192.168.10.9': 'Windows',
+     '192.168.10.5': 'Windows',
+     '192.168.10.8': 'Windows',
+     '192.168.10.14': 'Windows',
+     '192.168.10.15': 'Windows',
+     '192.168.10.25': 'macOS'
 }
 
-def data_preprocess(init_df):
+# Consider all Windows as Windows
+# ip_dict = {
+#     '192.168.10.51': 'Ubuntu server 12',
+#     '192.168.10.19': 'Ubuntu 14.4',
+#     '192.168.10.17': 'Ubuntu 14.4',
+#     '192.168.10.16': 'Ubuntu 16.4',
+#     '192.168.10.12': 'Ubuntu 16.4',
+#     '192.168.10.9': 'Windows',
+#     '192.168.10.5': 'Windows',
+#     '192.168.10.8': 'Windows',
+#     '192.168.10.14': 'Windows',
+#     '192.168.10.15': 'Windows',
+#     '192.168.10.25': 'macOS'
+# }
 
-    init_df.drop(columns=['ip.tos', 'tcp.options.mss_val'], inplace=True)
-    init_df.dropna(inplace=True)
-    init_df['tcp.srcport'] = np.where(init_df['tcp.srcport'] > 1024, 1, 0)
-    init_df['tcp.dstport'] = np.where(init_df['tcp.dstport'] > 1024, 1, 0)
 
-    """init_df['ip.dsfield'] = init_df['ip.dsfield'].apply(int, base=16)
-    init_df['tcp.flags'] = init_df['tcp.flags'].apply(int, base=16)
-    init_df['ip.flags'] = init_df['ip.flags'].apply(int, base=16)"""
+####################################################################
+########################## Option Set Up ###########################
+####################################################################
 
-    return init_df
+def option_check():
+    # all available argument options
+    avail_options = ["-file", "-features", "-label", "-test"]
+
+    # receive user given options
+    options = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+
+    # receive user given arguments
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+    # raise error if user given option is wrong
+    for i in options:
+        if i not in avail_options:
+            raise SystemExit(f"Usage: {sys.argv[0]} -file -features -label -test <arguments>...")
+
+    # raise error if not all options or arguments are available
+    if len(options) != 4 or len(args) != 4:
+        raise SystemExit(f"Usage: {sys.argv[0]} -file -features -label -test <arguments>...")
+
+    return args
 
 
-def add_label(df, ip_label_dict):
-    # create list of known IP addresses
-    known_ip_list = [key for key in ip_label_dict.keys()]
+####################################################################
+########################## Main Function ###########################
+####################################################################
 
-    # filter df based on known IP addresses
-    all_ip_list = list(set(df['ip.src'].tolist()))
-    unnecessary_ip_list = [item for item in all_ip_list if item not in known_ip_list]
-    filtered_df = df.copy()
-    for ip in unnecessary_ip_list:
-        filtered_df = filtered_df[filtered_df['ip.src'] != ip]
 
-    # add label to the filtered df
-    for ip in known_ip_list:
-        filtered_df.loc[filtered_df['ip.src'] == ip, "os"] = ip_label_dict[ip]
+if __name__ == "__main__":
 
-    # print(filtered_df)
+    # args = option_check()
+    # filename = args[0]
 
-    return filtered_df
+    filename = "dataset\\monday.csv"
+    df = pd.read_csv(filename)
+    processed_df = data_preprocess(df)
+    labeled_df = add_label(processed_df, ip_dict)
 
-if __name__ == '__main__':
+    print("Total number of packets: ", len(labeled_df))
+    print(labeled_df)
 
-    for i, filename in enumerate(os.listdir(directory)):
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            df = pd.read_csv(directory + '\\' + filename)
-            processed_df = data_preprocess(df)
-            labeled_df = add_label(processed_df, ip_dict)
-            if not i:
-                labeled_df.to_csv('.\\labeled.csv', mode='w', index=False)
-            else:
-                labeled_df.to_csv('.\\labeled.csv', mode='a+', header=False, index=False)
+    labeled_df.to_csv("dataset\\labeled_monday.csv", encoding='utf-8', index=False)
 
+
+    filename = "dataset\\labeled_monday.csv"
+    labeled_df = pd.read_csv(filename)
+    #
+    # # fingerprinting with classification
+    #
+    #  x_iloc_list = ['ip.ttl', 'ip.len', 'tcp.hdr_len', 'tcp.window_size', 'ip.flags.df', 'tcp.flags.syn',
+    # 'ip.hdr_len', 'tcp.flags.ack', 'tcp.flags.push', 'tcp.seq', 'tcp.len']
+    x_iloc_list = [8, 13, 20, 29, 5, 23, 1, 26, 25, 17, 19]  # indexes in the original monday csv
+    y_iloc = 32
+
+    testSize = float(0.2)
+    model = ClassifierModel(labeled_df, x_iloc_list, y_iloc, testSize)
+
+    filename = "dataset\\yair-mac-traffic.csv"
+    df = pd.read_csv(filename)
+    df = df.drop(columns=['ip.tos', 'tcp.options.mss_val'])
+    user_traffic = df.dropna()
+    x_iloc_list = [8, 13, 20, 29, 5, 23, 1, 26, 25, 17, 19]   # indexes in user's csv
+
+    model.KNN()
+    model.SVM('linear')
+    model.SVM('rbf')
+    model.NB()
+    model.RF()
+    model.ANN()
+
+
+    # model.run_models(user_traffic, x_iloc_list, 'macOS')
